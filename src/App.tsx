d4f6c5b28df6a796dd09caa7e37a032cfbacec71
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-11 22:32:16 +0200

    set basic structure for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './index.css'
-function App() {
+
+const App=()=> {
+
+  const startTrivia = async()=>{
+
+  }
+  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement>)=>{
+
+  }
+
+  const nextQuestion =()=>{
+
+  }
   return (
     <div className="App">
-      <h1>Quiz</h1>
+      <h1>React Quiz</h1>
+      <button className="start">Start</button>
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,1 +1,5 @@
 import React from 'react';
+
+const QuestionCard = () => <div>Question Card</div>
+
+export default QuestionCard;
