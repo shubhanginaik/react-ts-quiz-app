7a2e8d154ba8ba50ea2cc3190f3cafb2a638db01
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-13 10:54:00 +0200

    basic functionality in App.tsx is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './index.css'
+// components
+import QuestionCard from './components/QuestionCard';
+
 
 const App=()=> {
 
@@ -15,8 +18,16 @@ const App=()=> {
   }
   return (
     <div className="App">
-      <h1>React Quiz</h1>
-      <button className="start">Start</button>
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next
+      </button>
     </div>
   );
 }
