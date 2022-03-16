9592e1ed167e2c52e66653ce405ac09fbbbdf3cd
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-16 13:17:28 +0200

    start and question card is working

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App=()=> {
    const [loading,setLoading] = useState(false);
   const [questions,setquestions] = useState<QuestionState[]>([]);
   const [number,setNumber] = useState(0);
-  const [useAnswers,setUserAnswers] = useState<AnswerObject[]>([]);
+  const [userAnswers,setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver,setGameOver]= useState(true);
   
@@ -51,22 +51,29 @@ console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY))
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
+      {/* We want to show 'start' button only if gameover or useranswers length is equal to no of questions */}
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ?(
       <button className="start" onClick={startTrivia}>
         Start
       </button>
-      <p className="score">Score:</p>
-      <p>Loading Questions</p>
-      {/* <QuestionCard 
+      ): null}
+      {! gameOver ?<p className="score">Score:</p>:null}
+      {loading && <p>Loading Questions...</p>}
+      {!loading && !gameOver && (
+      <QuestionCard 
       questionNr = {number + 1}
       totalQuestions= {TOTAL_QUESTIONS}
       question={questions[number].question}
-      answers={questions[number].answer}
-      userAnswer={useAnswers? useAnswers[number] : undefined}
+      answers={questions[number].answers}
+      userAnswer={userAnswers? userAnswers[number] : undefined}
       callback={checkAnswer}
-      /> */}
+      />
+      )}
+      {!gameOver && !loading && userAnswers.length === number +1 && number !== TOTAL_QUESTIONS-1 ?(
       <button className="next" onClick={nextQuestion}>
         Next
       </button>
+      ) : null}
     </div>
   );
 }
